b53c18a4cb7134d483de6dc97f1c27baa077774c
Your Name <you@example.com>
2023-02-25 21:12:12 +0530

    Adding a welcome message

---
+++ LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Programs");
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
